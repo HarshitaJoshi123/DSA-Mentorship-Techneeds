9f353264d403409f471052dff4ec10cc19a38647
Harshita Joshi <144125889+HarshitaJoshi123@users.noreply.github.com>
2023-09-28 20:23:47 +0530

    Create A1_Q1.java

---
+++ A1_Q1.java
@@ -0,0 +1,12 @@
+package mentorship.assignment1;
+
+public class A1_Q1 {
+    public static void main(String[] args) {
+        //ques1 - Print z
+        System.out.println("*****");
+        System.out.println("   *");
+        System.out.println("  *");
+        System.out.println(" *");
+        System.out.println("*****");
+    }
+}
